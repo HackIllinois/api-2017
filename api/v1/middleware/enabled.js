573225fbb62b927236998aeb2f31c54e1cc3bec4
Arnav Sankaran <arnavsankaran@gmail.com>
2017-11-15 19:48:30 -0600

    Fixed comparism

--- api/v1/middleware/enabled.js
+++ api/v1/middleware/enabled.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   endpoint = url.parse(req.url).pathname;
 
   cache.get(endpoint, function(err, reply) {
-    if (reply == null || reply == true) {
+    if (reply == null || reply == 'true') {
       return next();
     } else {
       return next(new errors.EndpointNotAvailableError());
