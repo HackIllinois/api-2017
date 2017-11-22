aca6eef9d84abaf267142ed8841b844696c99a67
Arnav Sankaran <arnavsankaran@gmail.com>
2017-11-22 14:08:31 -0600

    Use cacheing for endpoint states

--- api/v1/controllers/EndpointController.js
+++ api/v1/controllers/EndpointController.js
@@ -7,28 +7,42 @@ const middleware = require('../middleware');
 
 const Endpoint = require('../models/Endpoint');
 
-// const cache = require('../../cache').instance();
+const cache = require('../../cache').instance();
 
 function modifyEndpointAccess(req, res, next) {
 
   if (req.body.enabled == null) {
     res.body = {};
     res.body.endpoint = req.body.endpoint;
-    Endpoint.query({where: {endpoint: req.body.endpoint}}).fetch().then((endpointModel) => {
-      res.body.enabled = (endpointModel == null || endpointModel.attributes.enabled[0] == 1);
-      return next();
+    cache.get(req.body.endpoint, (err, reply) => {
+      if (reply != null) {
+        res.body.enabled = (reply == 'true');
+        return next();
+      } 
+      Endpoint.query({where: {endpoint: res.body.endpoint}}).fetch().then((endpointModel) => {
+        if (endpointModel == null || endpointModel.attributes.enabled[0] == 1) {
+          cache.set(req.body.endpoint, 'true');
+          res.body.enabled = true;
+          return next();
+        } 
+        cache.set(req.body.endpoint, 'false');
+        res.body.enabled = false;
+        return next();
+          
+      });
+      
     });
   } else {
     // Write enabled / disabled state to cache
-    // cache.set(req.body.endpoint, req.body.enabled);
+    cache.set(req.body.endpoint, req.body.enabled);
 
     // Write enabled / disabled state to databas
     Endpoint.query({where: {endpoint: req.body.endpoint}}).fetch().then((endpointModel) => {
-      methodType = ((endpointModel == null) ? 'insert' : 'update');
+      methodType = ((endpointModel == null) ? 'insert' : 'update'); // eslint-disable-line no-undef
       Endpoint.forge({
         endpoint: req.body.endpoint,
         enabled: req.body.enabled
-      }).save(null, {method: methodType});
+      }).save(null, {method: methodType}); // eslint-disable-line no-undef
     });
     res.body = req.body;
     return next();
--- api/v1/middleware/enabled.js
+++ api/v1/middleware/enabled.js
@@ -1,26 +1,28 @@
 const errors = require('../errors');
-// const cache = require('../../cache').instance();
+const cache = require('../../cache').instance();
 
 const Endpoint = require('../models/Endpoint');
 
 const url = require('url');
 
 module.exports = (req, res, next) => {
-  endpoint = url.parse(req.url).pathname;
+  endpointPath = url.parse(req.url).pathname; // eslint-disable-line no-undef
 
-  // cache.get(endpoint, function(err, reply) {
-  //   if (reply == null || reply == 'true') {
-  //     return next();
-  //   } else {
-  //     return next(new errors.EndpointNotAvailableError());
-  //   }
-  // });
-
-  Endpoint.query({where: {endpoint: req.url}}).fetch().then((endpointModel) => {
-    if (endpointModel == null || endpointModel.attributes.enabled[0] == 1) {
+  cache.get(endpointPath, (err, reply) => { // eslint-disable-line no-undef
+    if (reply == 'true') {
       return next();
+    } else if (reply == 'false') {
+      return next(new errors.EndpointNotAvailableError());
     } 
-    return next(new errors.EndpointNotAvailableError());
+    Endpoint.query({where: {endpoint: endpointPath}}).fetch().then((endpointModel) => { // eslint-disable-line no-undef
+      if (endpointModel == null || endpointModel.attributes.enabled[0] == 1) {
+        cache.set(endpointPath, 'true'); // eslint-disable-line no-undef
+        return next();
+      } 
+      cache.set(endpointPath, 'false'); // eslint-disable-line no-undef
+      return next(new errors.EndpointNotAvailableError());
+        
+    });
     
   });
 
